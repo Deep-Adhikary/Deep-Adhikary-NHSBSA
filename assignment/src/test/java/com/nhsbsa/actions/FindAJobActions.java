a6769237ce9b71715569355ccc53381b2812d84d
Deep-Adhikary <adhikary33@gmail.com>
2024-05-13 23:04:53 +0530

    created new action to verify sorting

--- assignment/src/test/java/com/nhsbsa/actions/FindAJobActions.java
+++ assignment/src/test/java/com/nhsbsa/actions/FindAJobActions.java
@@ -1,14 +1,20 @@
 package com.nhsbsa.actions;
 
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.By;
+
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
+import com.google.common.base.Function;
+import com.google.common.collect.Ordering;
 import com.nhsbsa.base.Actions;
-import com.nhsbsa.base.FindAJobElements;
+import com.nhsbsa.elements.FindAJobElements;
 import com.nhsbsa.models.JobPreferences;
 import com.nhsbsa.stepdefinations.Context;
+import java.util.stream.Collectors;
+import java.util.List;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 import static org.testng.Assert.*;
 
@@ -25,11 +31,14 @@ public class FindAJobActions extends Actions {
         assertTrue(findAJobElements.getPageHeading().isDisplayed());
         takeScreenShotToFile();
     }
-    private void provideLocationPreference(String location){
-        if (location.equals("")) return;
+
+    private void provideLocationPreference(String location) {
+        if (location.equals(""))
+            return;
         findAJobElements.getWhereInput().sendKeys(location);
         findAJobElements.getLocationDropDownItem(location).click();
     }
+
     public void applyPreferences(JobPreferences preferences) {
 
         scrollIntoView(findAJobElements.getWhatInput());
@@ -67,26 +76,58 @@ public class FindAJobActions extends Actions {
     }
 
     public void valiDateAtlLeastOneSearchResultsReturned() {
-        
+
         assertNotEquals(findAJobElements.getSearchResults().size(), 0);
         assertTrue(
-            Integer.parseInt(findAJobElements.getSearchResultsCount().getText())>0);
-            scrollIntoView(findAJobElements.getSearchResultsCount());
+                Integer.parseInt(findAJobElements.getSearchResultsCount().getText()) > 0);
+        scrollIntoView(findAJobElements.getSearchResultsCount());
         pause(0.5);
         scrollIntoView(findAJobElements.getSearchResults().get(0));
 
         takeScreenShotToFile();
     }
-    public void sortSearchResult(String sortBy){
-        WebElement firstResult=findAJobElements.getSearchResults().get(0);
+
+    public void sortSearchResult(String sortBy) {
+        WebElement firstResult = findAJobElements.getSearchResults().get(0);
         findAJobElements.getSortByDropDown().selectByVisibleText(sortBy);
         wait.until(ExpectedConditions.stalenessOf(firstResult));
-        firstResult=findAJobElements.getSearchResults().get(0);
+        firstResult = findAJobElements.getSearchResults().get(0);
         scrollIntoView(firstResult);
         pause();
         takeScreenShotToFile();
     }
-    public void verifyNoResultsReturned(){
+
+    public void verifyDataSortedByDatePostedNewest() {
+        List<String> locators = findAJobElements
+                .getSearchResults()
+                .stream()
+                .map(
+                        searchedJob -> {
+                            return searchedJob.findElement(findAJobElements.getSearchResultTitle()).getAttribute("href")
+                                    .replace(baseUrl, "");
+                        })
+                .collect(Collectors.toList());
+        List<LocalDate> dates = locators.stream().map(locator -> {
+            DateTimeFormatter datePostedDateFormat = DateTimeFormatter.ofPattern("dd MMM yyyy");
+
+            WebElement element = wait
+                    .until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("a[href='" + locator + "']")));
+            scrollIntoView(element).click();
+            wait.until(ExpectedConditions.stalenessOf(element));
+
+            LocalDate date = LocalDate.parse(
+                    scrollIntoView(findAJobElements.getVisibleDatePostedLabel()).getAttribute("textContent"),
+                    datePostedDateFormat);
+            wait.until(ExpectedConditions.elementToBeClickable(findAJobElements.getBackToSearchButton())).click();
+            wait.until(ExpectedConditions.visibilityOf(findAJobElements.getSearchResultsCount()));
+            return date;
+        }).collect(Collectors.toList());
+
+        Function<List<LocalDate>,Boolean> dateComparator= dateList->Ordering.<LocalDate> natural().reverse().isOrdered(dateList);
+        assertTrue(dateComparator.apply(dates));
+    }
+
+    public void verifyNoResultsReturned() {
         scrollIntoView(findAJobElements.getNoResultsFoundText());
         assertTrue(findAJobElements.getNoResultsFoundText().isDisplayed());
         takeScreenShotToFile();
